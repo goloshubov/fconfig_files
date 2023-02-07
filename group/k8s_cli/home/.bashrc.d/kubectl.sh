549e2f11847cceeea060cd9b66cb9c234c0d9d7e
Dmitry Goloshubov <goloshubov@gmail.com>
2023-02-07 13:53:07 +0300

    edit .bashrc.d/kubectl.sh

--- group/k8s_cli/home/.bashrc.d/kubectl.sh
+++ group/k8s_cli/home/.bashrc.d/kubectl.sh
@@ -1,10 +1,3 @@
 alias k=kubectl
 source <(kubectl completion bash)
 complete -F __start_kubectl k
-
-alias kubectx=kubectl-ctx
-alias kctx=kubectl-ctx
-
-alias kubens=kubectl-ns
-alias kns=kubectl-ns
-
