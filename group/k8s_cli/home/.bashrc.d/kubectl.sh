a5c0322e13a03e0827a7cdda0faff12b1a41f6a5
Dmitry Goloshubov <goloshubov@gmail.com>
2023-02-27 18:28:57 +0300

    add bash aliases to kubectl.sh

--- group/k8s_cli/home/.bashrc.d/kubectl.sh
+++ group/k8s_cli/home/.bashrc.d/kubectl.sh
@@ -1,3 +1,8 @@
 alias k=kubectl
 source <(kubectl completion bash)
 complete -F __start_kubectl k
+
+alias k_no_gpu="kubectl get nodes -o=go-template='{{range .items}}{{.metadata.name}}{{printf \"\t\"}}{{.status.capacity.cpu}}{{printf \"\t\"}}{{.status.capacity.memory}}{{printf \"\t\"}}{{index .status.capacity \"nvidia.com/gpu\"}}{{printf \"\n\"}}{{end}}' | sort -n -k 4 -k 2"
+alias k_po_res="kubectl get pod -o jsonpath='{range .items[*]}{\"\nPod: \"}{.metadata.name}{\"\nNamespace: \"}{.metadata.namespace}{range .spec.containers[*]}{\"\n\tContainer: \"}{.name}{\"\n\"}{\"\t\tResources:\"}{.resources}{end}{\"\n\"}{end}'"
+alias k_po_img="kubectl get pod -o jsonpath='{range .items[*]}{\"\nPod: \"}{.metadata.name}{\"\nNamespace: \"}{.metadata.namespace}{range .spec.containers[*]}{\"\n\tContainer: \"}{.name}{\"\n\"}{\"\t\tImage:\"}{.image}{end}{\"\n\"}{end}'"
+alias k_rs_img="kubectl get rs -o jsonpath='{range .items[*]}{\"\nRS: \"}{.metadata.name}{\"\nNamespace: \"}{.metadata.namespace}{range .spec.template.spec.containers[*]}{\"\n\tContainer: \"}{.name}{\"\n\"}{\"\t\tImage:\"}{.image}{end}{\"\n\"}{end}'"
