3d5fae002b2a101d0bc87de1acbff5615b656eda
Dmitry Goloshubov <goloshubov@gmail.com>
2023-03-02 00:23:09 +0300

    add fconfig aliases

--- group/all/home/.bashrc.d/aliases.sh
+++ group/all/home/.bashrc.d/aliases.sh
@@ -16,7 +16,6 @@ FCONFIG_PULL='git -C ~/git/github/fconfig/ pull'
 FCONFIG_WS='ansible-playbook -i ~/git/github/fconfig/inventory_workstations.ini ~/git/github/fconfig/workstation.yml -e ansible_connection=local --limit $(hostname)'
 alias fconfig_local_ws="$FCONFIG_PULL && $FCONFIG_WS"
 alias fconfig_local_ws_dotfiles="$FCONFIG_PULL && $FCONFIG_WS --tags dotfiles"
-
 FCONFIG_SERVER='ansible-playbook -i ~/git/github/fconfig/inventory_servers.ini ~/git/github/fconfig/server.yml -e ansible_connection=local --limit $(hostname)'
 alias fconfig_local_srv="$FCONFIG_PULL && $FCONFIG_SERVER"
 alias fconfig_local_srv_dotfiles="$FCONFIG_PULL && $FCONFIG_SERVER --tags dotfiles"
