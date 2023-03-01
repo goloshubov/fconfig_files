5724ccde9e749df1ad1b174898b658c3f5f67ec6
Dmitry Goloshubov <goloshubov@gmail.com>
2023-03-01 23:23:07 +0300

    add fconfig aliases

--- group/all/home/.bashrc.d/aliases.sh
+++ group/all/home/.bashrc.d/aliases.sh
@@ -12,8 +12,8 @@ alias grep_teal='GREP_COLORS="ms=01;36" grep --color=auto'
 alias grep_lgrey='GREP_COLORS="ms=01;37" grep --color=auto'
 
 
-alias fconfig_local_ws='git pull /home/goloshubov/git/github/fconfig/ && ansible-playbook -i /home/goloshubov/git/github/fconfig/inventory_workstations.ini /home/goloshubov/git/github/fconfig/workstation.yml -e ansible_connection=local --limit $(hostname)'
-alias fconfig_local_ws_dotfiles='git pull /home/goloshubov/git/github/fconfig/ && ansible-playbook -i /home/goloshubov/git/github/fconfig/inventory_workstations.ini /home/goloshubov/git/github/fconfig/workstation.yml -e ansible_connection=local --limit $(hostname) --tags dotfiles'
+alias fconfig_local_ws='git pull ~/git/github/fconfig/ && ansible-playbook -i ~/git/github/fconfig/inventory_workstations.ini ~/git/github/fconfig/workstation.yml -e ansible_connection=local --limit $(hostname)'
+alias fconfig_local_ws_dotfiles='git pull ~/git/github/fconfig/ && ansible-playbook -i ~/git/github/fconfig/inventory_workstations.ini ~/git/github/fconfig/workstation.yml -e ansible_connection=local --limit $(hostname) --tags dotfiles'
 
-alias fconfig_local_srv='git pull /home/goloshubov/git/github/fconfig/ && ansible-playbook -i /home/goloshubov/git/github/fconfig/inventory_servers.ini /home/goloshubov/git/github/fconfig/server.yml -e ansible_connection=local --limit $(hostname)'
-alias fconfig_local_srv_dotfiles='git pull /home/goloshubov/git/github/fconfig/ && ansible-playbook -i /home/goloshubov/git/github/fconfig/inventory_servers.ini /home/goloshubov/git/github/fconfig/server.yml -e ansible_connection=local --limit $(hostname) --tags dotfiles'
+alias fconfig_local_srv='git pull ~/git/github/fconfig/ && ansible-playbook -i ~/git/github/fconfig/inventory_servers.ini ~/git/github/fconfig/server.yml -e ansible_connection=local --limit $(hostname)'
+alias fconfig_local_srv_dotfiles='git pull ~/git/github/fconfig/ && ansible-playbook -i ~/git/github/fconfig/inventory_servers.ini ~/git/github/fconfig/server.yml -e ansible_connection=local --limit $(hostname) --tags dotfiles'
