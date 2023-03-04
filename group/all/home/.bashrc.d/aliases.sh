0d35fd9f374b128de3b23e2eb920768d62cb8313
Dmitry Goloshubov <goloshubov@gmail.com>
2023-03-04 16:41:59 +0300

    Update aliases.sh

--- group/all/home/.bashrc.d/aliases.sh
+++ group/all/home/.bashrc.d/aliases.sh
@@ -1,6 +1,5 @@
 alias cpv='rsync -ah --info=progress2'
 
-
 alias grep_dgrey='GREP_COLORS="ms=01;30" grep --color=auto'
 alias grep_red='GREP_COLORS="ms=01;31" grep --color=auto'
 alias grep_red_blink='GREP_COLORS="ms=05;31;40" grep --color=auto'
@@ -11,7 +10,6 @@ alias grep_purple='GREP_COLORS="ms=01;35" grep --color=auto'
 alias grep_teal='GREP_COLORS="ms=01;36" grep --color=auto'
 alias grep_lgrey='GREP_COLORS="ms=01;37" grep --color=auto'
 
-
 FCONFIG_PULL='git -C ~/git/github/fconfig/ pull'
 FCONFIG_WS='ansible-playbook -i ~/git/github/fconfig/inventory ~/git/github/fconfig/workstation.yml -e ansible_connection=local --limit $(hostname)'
 FCONFIG_SERVER='ansible-playbook -i ~/git/github/fconfig/inventory ~/git/github/fconfig/server.yml -e ansible_connection=local --limit $(hostname)'
@@ -19,4 +17,3 @@ alias fconfig_local_ws="$FCONFIG_PULL && $FCONFIG_WS"
 alias fconfig_local_ws_dotfiles="$FCONFIG_PULL && $FCONFIG_WS --tags dotfiles"
 alias fconfig_local_srv="$FCONFIG_PULL && $FCONFIG_SERVER"
 alias fconfig_local_srv_dotfiles="$FCONFIG_PULL && $FCONFIG_SERVER --tags dotfiles"
-
