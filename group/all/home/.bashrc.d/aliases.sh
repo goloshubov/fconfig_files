93c4b38b11a712c2da86b4614768106315170e78
Dmitry Goloshubov <goloshubov@gmail.com>
2023-03-18 21:21:53 +0300

    add phosphor alias

--- group/all/home/.bashrc.d/aliases.sh
+++ group/all/home/.bashrc.d/aliases.sh
@@ -17,3 +17,8 @@ alias fconfig_local_ws="$FCONFIG_PULL && $FCONFIG_LOCAL_WS"
 alias fconfig_local_ws_dotfiles="$FCONFIG_PULL && $FCONFIG_LOCAL_WS --tags dotfiles"
 alias fconfig_local_srv="$FCONFIG_PULL && $FCONFIG_LOCAL_SERVER"
 alias fconfig_local_srv_dotfiles="$FCONFIG_PULL && $FCONFIG_LOCAL_SERVER --tags dotfiles"
+
+
+# JFF. xscreensaver phosphor like output (for cool-retro-term)
+export PHOSPHOR_PATH=/etc/os-release
+alias phosphor='while true; do for filename in $(find $PHOSPHOR_PATH -type f,l); do echo filename: $filename; array=$(cat $filename); for (( i=0; i<${#array}; i++ )); do echo -ne "${array:$i:1}"; sleep 0.01; done; done; done'
