89582db087cdd778f184ca3f9aa6f30f509dc208
Dmitry Goloshubov <goloshubov@gmail.com>
2024-05-14 10:35:27 +0300

    fix kubecolor autocompletion

--- group/k8s_cli/home/.bashrc.d/kubectl.sh
+++ group/k8s_cli/home/.bashrc.d/kubectl.sh
@@ -3,7 +3,7 @@ alias kk=kubecolor
 alias kaf="kubectl apply -f"
 
 source <(kubectl completion bash)
-complete -F __start_kubectl k
+complete -F __start_kubectl k kk
 
 alias k_no_res_gpu="kubectl get nodes -o=go-template='{{range .items}}{{.metadata.name}}{{printf \"\t\"}}{{.status.capacity.cpu}}{{printf \"\t\"}}{{.status.capacity.memory}}{{printf \"\t\"}}{{index .status.capacity \"nvidia.com/gpu\"}}{{printf \"\n\"}}{{end}}' | sort -n -k 4 -k 2"
 alias k_po_res="kubectl get pod -o jsonpath='{range .items[*]}{\"\nPod: \"}{.metadata.name}{\"\nNamespace: \"}{.metadata.namespace}{range .spec.containers[*]}{\"\n\tContainer: \"}{.name}{\"\n\"}{\"\t\tResources:\"}{.resources}{end}{\"\n\"}{end}'"
