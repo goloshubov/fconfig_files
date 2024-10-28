32d571a3c28f76b496ff6ad91ffa0c71bfb73c89
Dmitry Goloshubov <goloshubov@gmail.com>
2024-10-29 00:34:17 +0300

    update fzf config

--- group/all/home/.bashrc.d/fzf.sh
+++ group/all/home/.bashrc.d/fzf.sh
@@ -1,5 +1,5 @@
-export FZF_CTRL_R_OPTS="--preview-window hidden"
-export FZF_DEFAULT_OPTS="--cycle --preview 'bat --color=always {}'"
+#export FZF_CTRL_R_OPTS="--preview-window hidden"
+#export FZF_DEFAULT_OPTS="--cycle --preview-window hidden --preview 'bat --color=always {}'"
 export FZF_DEFAULT_COMMAND="fd --type f --hidden"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
