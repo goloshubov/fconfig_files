d3273a9bdc631874a1c51607400f6879cd20b57f
Dmitry Goloshubov <goloshubov@gmail.com>
2024-10-28 23:01:26 +0300

    add fzf

---
+++ group/all/home/.bashrc.d/fzf.sh
@@ -0,0 +1,7 @@
+#export FZF_DEFAULT_OPTS="--preview 'bat --color=always {}'"
+#export FZF_DEFAULT_COMMAND="fd --type f"
+#export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+source /usr/share/fzf/shell/key-bindings.bash
+
+alias vim_p='vim $(fzf --preview "bat --color=always {}")'
