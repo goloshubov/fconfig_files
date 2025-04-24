cc99aac225b85be3b7f76ab7ffd4e78b908805c0
Dmitry Goloshubov <goloshubov@gmail.com>
2025-04-24 04:08:18 +0300

    update JAVA_HOME, add ansible-rulebook (ansible venv)

--- group/workstations/home/.bashrc.d/java.sh
+++ group/workstations/home/.bashrc.d/java.sh
@@ -1,4 +1,4 @@
 #updated for ansible-rulebook
-export JDK_HOME=/usr/lib/jvm/java-17-openjdk
+export JDK_HOME=/usr/lib/jvm/java-21-openjdk
 export JAVA_HOME=$JDK_HOME
-export PIP_NO_BINARY=jpy
+#export PIP_NO_BINARY=jpy
