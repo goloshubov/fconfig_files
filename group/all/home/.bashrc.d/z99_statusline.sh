334ac56febd5380c3eaf2276cf9534090ea77360
Dmitry Goloshubov <goloshubov@gmail.com>
2025-04-27 18:34:48 +0300

    clear

--- group/all/home/.bashrc.d/z99_statusline.sh
+++ group/all/home/.bashrc.d/z99_statusline.sh
@@ -2,7 +2,7 @@
 
 # can be toggled by 'toggle STATUSLINE_K8S_SHOW' command
 # ~/.bashrc.d/function_toggle.sh
-# ~/.bashrc.d/00_toggle_envs.workstations.sh
+# ~/.bashrc.d/00_toggle_envs.sh
 STATUSLINE_K8S_SHOW="${STATUSLINE_K8S_SHOW:-0}"
 
 # ~/.bashrc.d/colors.sh
